607b96af9d1500c045c9688176bd9967e03b7fd9
Donny Kim <dkim8398@sdsu.edu>
2020-05-20 12:15:25 -0700

    Habitat area
    Pseudo code drfat

---
+++ R/habit.R
@@ -0,0 +1,39 @@
+#'
+#'#'
+#' @a coeff for initial funding & habitat area relationship
+#' @b coeff for maintenance funding & habitat area relationship
+#' @c coeff for habitat area change without action
+
+# Let's keep this function to return a list or matrix or dataframe, instead of going into solver...
+# I just wrote down random numbers and formula. Need to apply one that makes sense.
+# To keep funding more...legible, let's say we use numerical unit of 1,000? (e.g. 100 equals to 100k dollars)
+
+habitat_area = function(original_area=100, 
+                        init_mgmt=F, maint_mgmt=F, 
+                        init_funding=NA, maint_funding=NA,
+                        n_time,
+                        a=0.0002, b=0.0025, c= -0.05){
+  
+  habitat_area = c(original_area)
+  
+  for (i in 2:n_time){
+    
+    # calculating restored area for each time step?
+    if (init_mgmt == T & maint_mgmt == T){
+      restored_area = (1 + a*init_funding/n_time + b*maint_funding + c) * habitat_area[i-1]
+      } else if(init_mgmt == T & maint_mgmt == F){
+        restored_area = (1 + a*init_funding/n_time + c) * habitat_area[i-1]
+      } else if(init_mgmt == F & maint_mgmt == T){
+        restored_area = (1 + b*maint_funding + c)* habitat_area[i-1]
+      }else if (init_mgmt == F & maint_mgmt == F){
+        restored_area = (1 + c) * habitat_area[i-1]
+      } 
+    
+      habitat_area <- c(habitat_area, restored_area)
+  }
+  
+  return(habitat_area)
+}
+
+x <-habitat_area(1000, init_mgmt = T, maint_mgmt = T, init_funding = 300, maint_funding = 20, n_time = 30)
+print(x)
